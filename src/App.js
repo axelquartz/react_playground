f0a4084b15375248e1cfebc056eb22803127050a
axelquartz <axelarep@gmail.com>
2024-11-12 23:24:19 -0600

    Test components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { render } from "@testing-library/react";
+// import Nav from "./components/Nav";
+import { Nav } from "./components/Nav";
+
+function Heading() {
+  return <h1>This is an h1 heading</h1>;
+}
+
+function Body() {
+  return <h2>This is a body element</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Heading />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+function Nav() {
+  return (
+    <div>
+      <h1>THIS IS A NAV</h1>
+    </div>
+  );
+}
+
+// export default Nav;
+export { Nav };
